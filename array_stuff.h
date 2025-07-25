79f8282c82d77ed65392aceee1c15a0b67f6ecf8
osama mohamoud <wwwhdosama@gmail.com>
2025-07-25 16:53:23 +0300

    Auto commit: 2025-07-25 16:53:23

--- array_stuff.h
+++ array_stuff.h
@@ -7,14 +7,24 @@ using namespace std;
 {
 	
 	 // arrray input 
-	 void array_input(int arr[100], int length) {
+	 void array_input(int arr[100], int &length) {
 		
-		 cout << text;
+		
 		 for (int i = 0; i < length; i++) {
-			 cout << "enter array element " << i + 1 << " index:" << "[" << i << "]" << " ";
+			 cout << "\nenter array element " << i + 1 << " index:" << "[" << i << "]" << " ";
 			 cin >> arr[i];
 		 }
+		 
+	 }
+	 //array ouput 
+	 void array_output(int arr[100], int length ) {
+		 cout << "\narray output: " << "[ ";
+		 for (int o = 0; o < length; o++)
+		 {
+			 cout << arr[o] << " ";
+		 }
+		 cout << " ]" << "\n";
 	 }
 
-
+	 // fill array2
 }
