7279d357d99ed936210fc3a3c299c92f6f6dfaa7
osama mohamoud <wwwhdosama@gmail.com>
2025-07-25 18:02:29 +0300

    Auto commit: 2025-07-25 18:02:29

--- array_stuff.h
+++ array_stuff.h
@@ -7,6 +7,7 @@ using namespace std;
 
  namespace arraystuff
 {
+	
 	 // random generator
 	 int random(int from, int to) {
 		 return rand() % (to - from + 1) + from;
@@ -52,10 +53,28 @@ using namespace std;
 	 }
 
 	 //array filled with random number
+	 // length is indicating the number of elements you want
 	 void array_filled_with_random_numbers(int from, int to, int Anyarr[100], string text, int length) {
 		 for (int f = 0; f < length; f++) 
 			 Anyarr[f] = random(from, to);
 		 
+	 }
+ 
+	 //get max number in array 
+	 int max_number_in_array(int Anyarr[100], int length) {
+		 int max = Anyarr[0];
+		 for (int m = 0; m < length; m++)
+			 if (Anyarr[m] > max) max = Anyarr[m];
+		 return max;
+	 }
+
+
+	 // get min number in array
+	 int min_number_in_array(int Anyarr[100], int length) {
+		 int min = Anyarr[0];
+		 for (int m = 0; m < length; m++)
+			 if (Anyarr[m] < min) min = Anyarr[m];
+		 return min;
 	 }
 
 }
