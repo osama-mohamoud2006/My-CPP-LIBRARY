102f88d5f7d8e00977ec1466c3aa6ae234fc3e1a
osama mohamoud <wwwhdosama@gmail.com>
2025-07-26 14:00:06 +0300

    Auto commit: 2025-07-26 14:00:06

--- logic_stuff.h
+++ logic_stuff.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include <cs>
+#include <cstdlib>
 using namespace std;
 namespace logic_stuff 
 {
@@ -14,9 +14,85 @@ namespace logic_stuff
 		else return false;
 	}
 
-	// generate random number {
-	double random(double from, double to) {
-		return rand()
+	// check even only 
+	bool is_even(int number) {
+		return (number % 2 == 0);
 	}
+
+	// check odd only
+	bool is_odd(int number) {
+		return (number % 2 != 0);
+	}
+
+	// is prime? 
+	bool prime_or_not(int number) {
+		if (number <= 1) return false;
+		int untill = number / 2;
+		for (int i = 2; i <= untill; i++) {
+			if (number % i == 0) return false;
+		}
+		return true;
+	}
+
+	
+	// generate random number {
+	double random(int to, int from) {
+		return rand() %(to - from + 1) + from;
+	}
+
+	// generate random values according to enum 
+	enum enCharType { smallLetter = 1, capitalLetter = 2, specialCharacter = 3, digit = 4 };
+	char get_random_stuff(enCharType CharType)
+	{
+		switch (CharType)
+		{
+		case enCharType::smallLetter:
+		{
+			return char(random(97, 122));
+			break;
+		}
+		case enCharType::capitalLetter:
+		{
+			return char(random(65, 90));
+			break;
+		}
+		case enCharType::specialCharacter:
+		{
+			return char(random(33, 47));
+			break;
+		}
+		case enCharType::digit:
+		{
+			return char(random(48, 57));
+			break;
+		}
+		}
+	}
+
+	// reverse number
+	int reversed_number(int Number) {
+		int Remainder = 0;
+		int Number2 = 0;
+		while (Number > 0)
+		{
+			Remainder = Number % 10;
+			Number = Number / 10;
+			Number2 = Number2 * 10 + Remainder;
+		}
+
+		return Number2;
+	}
+
+	// simple calaculator
+	enum enoperation {add=1 , sub=2 , div=3 , mod=4 , multi=5};
+	double calculator(double number1, double number2 , enoperation type) {
+		switch (type) {
+		case::add:
+			return number1 + number2;
+			case::
+		}
+	}
+
+	// convert char to int
 
 }
